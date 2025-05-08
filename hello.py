453aa5d58968490d24e1e3c42ee7b8a6c323420c
JoshZenith <josh.hart@zenith-semi.com>
2025-05-08 10:01:30 -0700

    Finished the first practice python file

--- hello.py
+++ hello.py
@@ -1,1 +1,47 @@
-print("Hello, World!")
+# This is a simple Python script that prints "Hello, World!" to the console.
+print("Hello, World!") #This is a comment
+
+"""
+This is a comment
+written in
+more than just one line
+"""
+
+# Variables
+x = 5
+y = "John"
+print(x)
+print(y)
+print(x + 3) # This will print 8
+
+# Assigning multiple variables
+x, y, z = "Orange", "Banana", "Cherry"
+print(x)
+print(y)
+print(z)
+
+x = y = z = "Orange"
+print(x)
+print(y)
+print(z)
+
+fruits = ["apple", "banana", "cherry"]
+x, y, z = fruits
+print(x)
+print(y)
+print(z)
+
+# Output variables
+x = "Python"
+y = "is"
+z = "awesome"
+print(x, y, z)
+
+x = "Python "
+y = "is "
+z = "awesome"
+print(x + y + z)
+
+x = 5
+y = "John"
+print(x, y)
